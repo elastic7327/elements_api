2042ba6aa040a24a63008a5e5966bf6faf494a35
elastic7327 <elastic7327@gamil.commit>
2017-03-01 17:27:45 +0800

    added comment

--- elements/engine/models.py
+++ elements/engine/models.py
@@ -1,5 +1,4 @@
 import time
-import re
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -7,8 +6,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+# function for uploaded file path
 def upload_path_handler(instance, filename):
     import os.path
+    # do split filename->(filename , extention)
     fn, ext = os.path.splitext(filename)
     return "{date}/{date}_{timestamp}{ext}".format(
         date=instance.date, timestamp=int(
@@ -16,13 +17,21 @@ def upload_path_handler(instance, filename):
 
 
 class Csv(models.Model):
+    """
+    is_archived  : True  => csv contents has been successfully saved
+    is_archived  : False => error ouccured
+
+    error_status : 0    => no error
+    error_status : 100  => file extention or format error
+    error_status : 200  => csv header error
+    """
     user = models.ForeignKey(User)
-    file = models.FileField(null=True, upload_to='uploaded_by_user/%Y/%m/%d')
+    file = models.FileField(null=True, upload_to=upload_path_handler)
     date = models.DateField(null=True)
     modified_at = models.DateField(auto_now=True, null=True)
     uploaded_at = models.DateTimeField(auto_now=True, null=True)
     is_archived = models.BooleanField()
-    error_status = models.IntegerField(default=0,null=False) #100 wrong:header missing | 200 wrong:fileformat
+    error_status = models.IntegerField(default=0, null=False)
 
     class Meta:
         managed = True
@@ -34,11 +43,13 @@ class Content(models.Model):
     description = models.CharField(max_length=256, null=True, blank=True)
     image = models.CharField(max_length=255, null=False, blank=True)
 
+
+    # get filtered contents by regular expression etc..
     @classmethod
     def get_clean_contents(self):
         pass
 
-    def as_json(self):
+    def as_json(self):  # get JSON style data
         return(
             {
                 "title": self.title,
@@ -46,9 +57,6 @@ class Content(models.Model):
                 "image": self.image,
             }
         )
-
-    def filepath(self):
-        return "/".join(filter(lambda x: x, [self.type, self.file.name]))
 
     class Meta:
         managed = True
