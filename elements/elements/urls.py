33ee6bac2f8a67fe947d13688b0469a79ff85262
Daniel KIM <jhe7327@naver.com>
2017-03-01 11:59:53 +0800

    rebug toolbar config on

--- elements/elements/urls.py
+++ elements/elements/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls import patterns
 from django.conf.urls.static import static
 
 
@@ -25,6 +24,11 @@ urlpatterns = [
     url(r'^engine/', include('engine.urls')),
 ]
 
-
 if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+    urlpatterns += [
+                url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
--- elements/script.sh
+++ elements/script.sh
@@ -1,7 +1,11 @@
 
 
-python manage.py migrate engine --settings=elements.settings.testing
-python manage.py makemigrations engine --settings=elements.settings.testing
 
-python manage.py runserver --settings=elements.settings.testing
+python manage.py makemigrations  --settings=elements.settings.testing
+python manage.py migrate --settings=elements.settings.testing
+
+python manage.py makemigrations engine --settings=elements.settings.testing
+python manage.py migrate engine --settings=elements.settings.testing
+
+python manage.py runserver 7000 --settings=elements.settings.testing
 #python manage.py shell_plus --settings=elements.settings.testing
